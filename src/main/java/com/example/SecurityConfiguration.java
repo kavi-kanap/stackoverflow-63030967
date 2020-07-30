3e55dba1c5b80e617dfdd26c7f1bfd8d77f48796
kavi-kanap <kavi.kanap@gmail.com>
2020-07-30 09:09:22 +0100

    Update SecurityConfiguration.java

--- src/main/java/com/example/SecurityConfiguration.java
+++ src/main/java/com/example/SecurityConfiguration.java
@@ -1,12 +1,9 @@
 package com.example;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
@@ -16,12 +13,10 @@ import java.util.Map;
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfiguration {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(SecurityConfiguration.class);
+    
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        LOGGER.debug("Initializing the security configuration");
         return http.authorizeExchange()
                 .pathMatchers("/secured").hasRole("USER")
                 .anyExchange().permitAll()
@@ -31,14 +26,8 @@ public class SecurityConfiguration {
 
     @Bean
     public CustomMapReactiveUserDetailsService userDetailsService() {
-        LOGGER.debug("Initializing the user details service");
-        UserDetails user1 = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
+        // There are no registered users at this point
         Map<String,  UserDetails> users = new HashMap<>();
-        users.put(user1.getUsername(), user1);
         return new CustomMapReactiveUserDetailsService(users);
     }
 }
